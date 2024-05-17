738ebde46018690df623b2815caad5f2f954689e
Katherine <f.katttherine@gmail.com>
2024-05-17 22:09:42 +0300

    //

---
+++ HuffmanArch/Huffman.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Course
+{
+    public class Huffman
+    {
+        public void CompressFile(string dataFilename, string archFilename)
+        {
+            byte[] data = File.ReadAllBytes(dataFilename);
+            byte[] arch = CompressBytes(data);
+            File.WriteAllBytes(archFilename, arch);
+        }
+
+        private byte[] CompressBytes(byte[] data)
+        {
+            int[] freqs = CalculateFreq(data);
+            Node root = CreateHuffmanTree(freqs);
+            string[] codes = CreateHuffmanCode(root);
+            byte[] bits = Compress(data, codes);
+            byte[] header = CreateHeader(data.Length, freqs);
+            return header.Concat(bits).ToArray();
+        }
+
+        private byte[] CreateHeader(int datalength, int[] freqs)
+        {
+            List<byte> header = new List<byte>();
+            //запись длины 4 байта
+            header.Add((byte)(datalength & 255));//младшие биты последние 8
+            header.Add((byte)((datalength >> 8) & 255));
+            header.Add((byte)((datalength >> 16) & 255));
+            header.Add((byte)((datalength >> 24) & 255));
+
+            for (int j = 0; j < 256; j++)
+                header.Add((byte)freqs[j]);
+            return header.ToArray();
+        }
+        private byte[] Compress(byte[] data, string[] codes)
+        {
+            List<byte> bits = new List<byte>();
+            byte sum = 0;
+            byte bit = 1;
+            foreach (byte symbol in data)
+                foreach (char c in codes[symbol])
+                {
+                    if (c == '1') sum |= bit;
+                    if (bit < 128) bit <<= 1;
+                    {
+                        bits.Add(sum);
+                        sum = 0;
+                        bit = 1;
+                    }
+
+                }
+            if (bit > 1) bits.Add(sum);
+            return bits.ToArray();
+        }
+        private string[] CreateHuffmanCode(Node root)
+        {
+            string[] codes = new string[256];
+            Next(root, "");
+            return codes;
+
+            void Next(Node node, string code)
+            {
+                if (node.bit0 == null)
+                    codes[node.symbol] = code;
+                else
+                {
+                    Next(node.bit0, code + "0");
+                    Next(node.bit1, code + "1");
+                }
+            }
+        }
+        private int[] CalculateFreq(byte[] data)
+        {
+            int[] freqs = new int[256];
+            foreach (byte d in data)
+                freqs[d]++;
+            return freqs;
+        }
+        private Node CreateHuffmanTree(int[] freqs)
+        {
+            //Приоритетная очередь:частота+узел(его номер + частотa)
+            PriorityQueue<Node> pq = new PriorityQueue<Node>();
+            for (int i = 0; i < 256; i++)
+            {
+                if (freqs[i] > 0)
+                    pq.Enqueue(freqs[i], new Node((byte)i, freqs[i]));
+            }
+            while (pq.Size() > 1)
+            {
+                Node bit0 = pq.Dequeue();
+                Node bit1 = pq.Dequeue();
+                int freq = bit0.freq + bit1.freq;
+                Node next = new Node(freq, bit0, bit0);
+                pq.Enqueue(freq, next);
+
+            }
+
+            return pq.Dequeue();
+        }
+    }
+}
---
+++ form/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.btnCompress = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(150, 78);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "button1";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // btnCompress
+            // 
+            this.btnCompress.Location = new System.Drawing.Point(355, 78);
+            this.btnCompress.Name = "btnCompress";
+            this.btnCompress.Size = new System.Drawing.Size(75, 23);
+            this.btnCompress.TabIndex = 1;
+            this.btnCompress.Text = "button2";
+            this.btnCompress.UseVisualStyleBackColor = true;
+            this.btnCompress.Click += new System.EventHandler(this.btnCompress_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(150, 156);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(100, 20);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnCompress);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button btnCompress;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox txtFilePath;
+    }
+}
+
